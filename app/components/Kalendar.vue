8bb461389dd36c833a1ab7b1319ad51421ab6e76
Alexey Orlov <30434540+Arcadnick@users.noreply.github.com>
2025-11-28 14:07:27 +0300

    Update Kalendar.vue

--- app/components/Kalendar.vue
+++ app/components/Kalendar.vue
@@ -234,7 +234,8 @@ function getMatchStatus(match) {
     if (
         match.date === '2025-11-09' ||
         match.date === '2025-11-15' ||
-        match.date === '2025-11-23'
+        match.date === '2025-11-23' ||
+        match.date === '2025-11-29'
     ) {
         return 'Перенесен'
     }

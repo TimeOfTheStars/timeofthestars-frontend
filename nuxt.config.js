ebcf15b00364f84bba0699ceede1784d607ef9d4
Alexey <Arcadnick@yandex.ru>
2026-01-29 20:21:00 +0300

    qwe

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     ssr: true,
     nitro: {
-        preset: 'node',
+        preset: 'static',
         crawlLinks: true,
     },
     css: ['~/assets/main.css'],
